925bcc31ff0137c18d0abd68a1c9263166a7253a
Sohee Moon <soheetech@gmail.com>
2023-11-28 18:33:00 +0900

    환경 설정 및 테스트

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,7 @@
 /**
  * @type {import('gatsby').GatsbyNode['createPages']}
  */
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
@@ -14,5 +15,23 @@ exports.createPages = async ({ actions }) => {
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+}
+
+const path = require("path")
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
   })
 }
